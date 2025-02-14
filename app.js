bd41cb2fb67b6711a1e648282fb68bf34a6b3d83
Matheus Cavalcante <matheuscavalcantes.mc@gmail.com>
2025-02-14 15:05:57 -0300

    finished building the core of the backend: fetching a question and its answers, then retrieving it all in a single object

--- app.js
+++ app.js
@@ -4,35 +4,74 @@ const sqlite3 = require("sqlite3").verbose();
 const app = express();
 const PORT = 3000;
 
-app.get("/api", (req, res) => {
-  res.send("hello, world!");
-});
+function generateRandomIntInRange(a, b) {
+  return a + Math.floor(b * Math.random());
+}
 
-app.get("/api/countQuestions", (req, res) => {
+function countQuestions() {
+  let count = undefined;
   const db = new sqlite3.Database("./database.db");
 
   db.serialize(() => {
     db.get(`SELECT COUNT(*) AS count FROM "Question"`, (err, row) => {
-      res.send({ count: row.count });
+      count = row.count;
     });
   });
 
   db.close();
-});
+  return count;
+}
 
-app.get("/api/countAnswers", (req, res) => {
+function countAnswers() {
+  let count = undefined;
   const db = new sqlite3.Database("./database.db");
 
   db.serialize(() => {
     db.get(`SELECT COUNT(*) AS count FROM "Answer"`, (err, row) => {
-      res.send({ count: row.count });
+      count = row.count;
     });
   });
 
   db.close();
+  return count;
+}
+
+app.get("/api/getQuestionAndAnswers", (req, res) => {
+  const questionId = generateRandomIntInRange(1, countQuestions());
+
+  const db = new sqlite3.Database("./database.db");
+  let payload = {
+    question: undefined,
+    answers: [],
+  };
+
+  db.serialize(() => {
+    db.get(`SELECT id, difficulty, content FROM "Question" WHERE id=${questionId}`, (err, row) => {
+      const question = {
+        id: row.id,
+        difficulty: row.difficulty,
+        content: row.content,
+      }
+
+      payload.question = question;
+    });
+
+    db.each(`SELECT id, questionId, content, isCorrect FROM "Answer" WHERE questionId=${questionId}`, (err, row) => {
+      const answer = {
+        id: row.id,
+        questionId: row.questionId,
+        content: row.content,
+        isCorrect: row.isCorrect,
+      }
+
+      payload.answers.push(answer);
+    });
+
+    res.send(payload);
+  });
+
+  db.close();
 });
-
-
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
