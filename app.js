d0e1423d575b781674f7b35c24a337e6498a79af
Matheus Cavalcante <matheuscavalcantes.mc@gmail.com>
2025-02-16 15:26:31 -0300

    now we have the response object

--- app.js
+++ app.js
@@ -6,6 +6,31 @@ const PORT = 3000;
 
 const db = new sqlite3.Database("./database.db");
 
+app.get("/api", (req, res) => {
+  db.serialize(() => {
+    db.get(`SELECT COUNT(*) AS count FROM "Question"`, (err, row) => {
+      const numberOfQuestions = row.count;
+      const questionId = 1 + Math.floor(numberOfQuestions * Math.random());
+
+      db.get(`SELECT difficulty, content FROM "Question" WHERE id = ?`, [questionId], (err, row) => {
+        const question = {
+          difficulty: row.difficulty,
+          content: row.content,
+        };
+
+        db.all(`SELECT content, isCorrect FROM "Answer" WHERE questionId = ?`, [questionId], (err, rows) =>{
+          const payload = {
+            question: question,
+            answers: rows,
+          };
+
+          res.json(payload);
+        });
+      });
+    });
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`PORT: ${PORT}`);
 });
